b0c137e80ad0af3da3f6d4cbf68322943da748c9
3amMcspicy <kc3265@columbia.edu>
2022-04-12 20:56:49 +0800

    added torus

--- website/main.js
+++ website/main.js
@@ -13,11 +13,18 @@ const renderer = new THREE.WebGLRenderer({ antialias: true, canvas: document.que
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight * 0.9);
 
-camera.position.setZ(10);
+camera.position.setZ(14);
 camera.position.setX(0);
-camera.position.setY(5);
+camera.position.setY(7);
 
+//Orbit controls and limitation on what users can do. 
 const controls = new OrbitControls(camera, renderer.domElement);
+controls.autoRotate = true;
+controls.autoRotateSpeed = 0.9;
+controls.enableZoom = false;
+controls.minPolarAngle = Math.PI / 3.7;
+controls.maxPolarAngle = Math.PI / 2.7;
+controls.autoRotateSpeed = 1.7;
 
 //Helpers
 const gridHelper = new THREE.GridHelper(200, 50);
@@ -27,22 +34,33 @@ const light = new THREE.AmbientLight(0x404040);
 scene.add(light);
 
 //globe
-const geometry = new THREE.SphereGeometry(3, 32, 16);
+const geometry = new THREE.SphereGeometry(3, 15, 16);
 const material = new THREE.MeshBasicMaterial({ color: 0xFC4343, wireframe: true });
 const sphere = new THREE.Mesh(geometry, material);
 sphere.position.set(0, 4, 0);
 scene.add(sphere);
+
 //dna
 
-//jazz?
+//torus for blender
+const donut_shape = new THREE.TorusGeometry(3, 1, 14, 20);
+const donut_flavour = new THREE.MeshBasicMaterial({ color: 0xFC4343, wireframe: true });
+const donut = new THREE.Mesh(donut_shape, donut_flavour);
+donut.position.set(10, 4, 0);
+scene.add(donut);
+
 
 animate();
 
 function animate() {
     requestAnimationFrame(animate);
 
-    sphere.rotation.y += 0.01;
-    sphere.rotation.z += 0.001;
+    // sphere.rotation.y += 0.005;
+    // sphere.rotation.z += 0.001;
+
+    donut.rotation.y += 0.002;
+    donut.rotation.z += 0.003;
+    donut.rotation.z += 0.002;
 
     controls.update();
 
